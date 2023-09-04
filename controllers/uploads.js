ffc803ccfc0407313ec5da2ff2ba6d540552fccd
canamend <isaac.canamend@gmail.com>
2023-09-04 01:09:13 -0600

    Function that handles images with cloudinary added

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,7 +1,10 @@
 const path = require('path');
 const fs = require('fs');
 
+const cloudinary = require('cloudinary').v2
+cloudinary.config( process.env.CLOUDINARY_URL );
 const { response } = require("express");
+
 const { uploadFile } = require('../helpers');
 const { User, Product } = require('../models');
  
@@ -61,6 +64,55 @@ const updateFile = async (req, res = response) =>{
   res.json(model);
 }
 
+const updateFileCloudinary = async (req, res = response) =>{
+
+  const { id, colection } = req.params;
+  let model;
+
+  switch ( colection ) {
+
+    case 'users':
+      model = await User.findById(id);
+      if(!model){
+        return res.status(400).json({
+          msg: `No existe un usuario con el id ${id}`
+        });
+      }
+    break;
+
+    case 'products':
+      model =  await Product.findById(id);
+      if(!model){
+        return res.status(400).json({
+          msg: `No existe un producto con el id ${id}`
+        })
+      }
+    break;
+  
+    default:
+      return res.status(500).json({ msg: 'Se me olvidó validar esto' });
+  }
+
+  // Limpiar imágenes previas
+  if( model.img ){
+    const nombreArr = model.img.split('/');
+    const nombre = nombreArr[ nombreArr.length - 1 ];
+    const [ public_id ] = nombre.split('.');
+    cloudinary.uploader.destroy(public_id);
+  }
+
+  const { tempFilePath } = req.files.file;
+
+  const { secure_url } = await cloudinary.uploader.upload( tempFilePath );
+
+  model.img = secure_url;
+
+  await model.save();
+
+  res.json( model );
+}
+
+
 
 showImage = async(req, res = response) => {
   
@@ -108,4 +160,5 @@ module.exports = {
     loadFile,
     showImage,
     updateFile,
+    updateFileCloudinary
 }
