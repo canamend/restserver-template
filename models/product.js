01fb673512eaee8023a539c1e9985b6085e1b8ad
canamend <isaac.canamend@gmail.com>
2023-09-04 00:34:42 -0600

    Image field added to product model

--- models/product.js
+++ models/product.js
@@ -27,6 +27,7 @@ const ProductSchema = Schema({
     },
     description: { type: String },
     disponible: { type: Boolean, default: true },
+    img: { type: String }
 });
 
 ProductSchema.methods.toJSON = function() {
