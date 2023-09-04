b8643e7a351eabd95b1fbba02ed5b5462286f284
canamend <isaac.canamend@gmail.com>
2023-09-04 00:34:15 -0600

    Middleware to check if file is added in the body request

---
+++ middlewares/validar-file.js
@@ -0,0 +1,12 @@
+
+const validateFile = ( req, res = response, next )  => {
+  if (!req.files || Object.keys(req.files).length === 0 || !req.files.file) {
+    return res.status(400).json({msg: 'No hay archivos que subir - archivo'});
+  }
+
+  next();
+}
+
+module.exports = {
+    validateFile
+}
