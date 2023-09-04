13ea1c1080363a9253e4f73dce0261478f3f426e
canamend <isaac.canamend@gmail.com>
2023-09-04 00:33:35 -0600

    File validator added to validators' index

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,9 +2,11 @@
 const validaCampos = require('../middlewares/validar-campos');
 const validaJWT = require('../middlewares/validar-jwt');
 const validaRoles = require('../middlewares/validar-roles');
+const validateFile = require('../middlewares/validar-file');
 
 module.exports = {
     ...validaCampos,
     ...validaJWT,
-    ...validaRoles
+    ...validaRoles,
+    ...validateFile
 }
